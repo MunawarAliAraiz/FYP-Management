e2f5baa117c3842bae9315cb54e000920771ca81
Munawar Ali <81907855+MunawarAliAraiz@users.noreply.github.com>
2022-02-18 20:02:46 +0500

    Main Window

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,201 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 518);
+            this.panel1.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Indigo;
+            this.button8.Location = new System.Drawing.Point(414, 378);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(226, 116);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Mark Evaluation";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.MouseEnter += new System.EventHandler(this.button8_MouseEnter);
+            this.button8.MouseLeave += new System.EventHandler(this.button8_MouseLeave);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Indigo;
+            this.button9.Location = new System.Drawing.Point(172, 378);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(233, 116);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Manage Evaluation";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.MouseEnter += new System.EventHandler(this.button9_MouseEnter);
+            this.button9.MouseLeave += new System.EventHandler(this.button9_MouseLeave);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Indigo;
+            this.button6.Location = new System.Drawing.Point(542, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(221, 116);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Assign Advisor";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.MouseEnter += new System.EventHandler(this.button6_MouseEnter);
+            this.button6.MouseLeave += new System.EventHandler(this.button6_MouseLeave);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Indigo;
+            this.button5.Location = new System.Drawing.Point(301, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(233, 116);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Assign Project";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+            this.button5.MouseLeave += new System.EventHandler(this.button5_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Indigo;
+            this.button4.Location = new System.Drawing.Point(60, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(233, 116);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Create Group";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Indigo;
+            this.button3.Location = new System.Drawing.Point(542, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(221, 116);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Manage Projects";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Indigo;
+            this.button2.Location = new System.Drawing.Point(301, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 116);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Manage Advisors";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Indigo;
+            this.button1.Location = new System.Drawing.Point(60, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 116);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Manage Students";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Indigo;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(221, 37);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(404, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FYP Management";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FYP Management";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,99 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.button1.ForeColor = System.Drawing.Color.Indigo;
+            this.button1.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            this.button2.ForeColor = System.Drawing.Color.Indigo;
+            this.button2.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            this.button3.ForeColor = System.Drawing.Color.Indigo;
+            this.button3.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.BackColor = System.Drawing.Color.Indigo;
+        }
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            this.button4.ForeColor = System.Drawing.Color.Indigo;
+            this.button4.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button5_MouseLeave(object sender, EventArgs e)
+        {
+            this.button5.ForeColor = System.Drawing.Color.Indigo;
+            this.button5.BackColor = System.Drawing.Color.White;
+        }
+        private void button6_MouseEnter(object sender, EventArgs e)
+        {
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button6_MouseLeave(object sender, EventArgs e)
+        {
+            this.button6.ForeColor = System.Drawing.Color.Indigo;
+            this.button6.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button8_MouseEnter(object sender, EventArgs e)
+        {
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button8_MouseLeave(object sender, EventArgs e)
+        {
+            this.button8.ForeColor = System.Drawing.Color.Indigo;
+            this.button8.BackColor = System.Drawing.Color.White;
+        }
+
+        private void button9_MouseEnter(object sender, EventArgs e)
+        {
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.BackColor = System.Drawing.Color.Indigo;
+        }
+
+        private void button9_MouseLeave(object sender, EventArgs e)
+        {
+            this.button9.ForeColor = System.Drawing.Color.Indigo;
+            this.button9.BackColor = System.Drawing.Color.White;
+        }
     }
 }
